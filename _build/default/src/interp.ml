c113ce594e20b4fd342b127298a9b79c8c9e07b0
Agrane Benlalam Adam <adam-christophe.agrane-benlalam@etu.u-paris.fr>
2024-10-25 21:01:03 +0200

    Implémentation des fonctions des questions suivantes : 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 4.0, 5.0 et 5.3 . J'ai également implémenté les fonctions : 4.3, 4.7 et 5.4 (tous les target_reached...). Mais elles seront surement à revoir car on ne peut pas les tester pour l'instant.

---
+++ _build/default/src/interp.ml
@@ -0,0 +1,38 @@
+open Geo
+
+(* Code de la Section 4 du projet. *)
+
+type instruction =
+  Move of transformation
+| Repeat of int * program
+| Either of program * program 
+and program = instruction list
+
+let rec is_deterministic (prog : program) : bool =
+  match prog with
+  |[] -> true
+  |hd::tl -> match hd with
+    |Move _ -> is_deterministic tl
+    |Repeat (_,repeat_prog) -> is_deterministic repeat_prog
+    |Either _ -> false
+
+let unfold_repeat (prog : program) : program =
+  failwith "À compléter"
+
+let run_det (prog : program) (p : point) : point list =
+  failwith "À compléter"
+
+let target_reached_det (prog : program) (p : point) (target : rectangle) : bool =
+  assert (is_deterministic prog);
+  in_rectangle target (List.nth (run_det prog p) ((List.length prog) - 1))
+  
+let run (prog : program) (p : point) : point list =
+  failwith "À compléter"
+
+let all_choices (prog : program) : program list =
+  failwith "À compléter"
+
+let target_reached (prog : program) (p : point) (r : rectangle) : bool =
+  assert (not (is_deterministic prog));
+  let possible_execution_list = all_choices prog in
+  List.for_all (fun sub_prog -> in_rectangle r (List.nth (run sub_prog p) ((List.length sub_prog) - 1))) possible_execution_list
