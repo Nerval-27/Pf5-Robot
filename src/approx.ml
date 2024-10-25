1e3f8ada87bee0a3818396838530cabd6a97f0c6
Agrane Benlalam Adam <adam-christophe.agrane-benlalam@etu.u-paris.fr>
2024-10-25 21:04:21 +0200

    Même commit car j'ai oublié de git add les fichiers ayant été modifié

--- src/approx.ml
+++ src/approx.ml
@@ -4,7 +4,8 @@ open Interp
 (* Code de la Section 5 du projet. *)
 
 let sample (rect : rectangle) : point =
-  failwith "À compléter"
+  {x = (Random.float (rect.x_max -. rect.x_min)) +. rect.x_min ;
+   y = (Random.float (rect.y_max -. rect.y_min)) +. rect.y_min}
   
 let transform_rect (t : transformation) (r : rectangle) : rectangle =
   failwith "À compléter"
@@ -13,10 +14,13 @@ let run_rect (prog : program) (r : rectangle) : rectangle list =
   failwith "À compléter"
 
 let inclusion (r : rectangle) (t : rectangle) : bool =
-  failwith "À compléter"
+  let corners_list = corners r in 
+  List.for_all (fun v -> in_rectangle t v) corners_list
 
 let target_reached_rect (prog : program) (r : rectangle) (target : rectangle) : bool =
-  failwith "À compléter"
+  assert (not (is_deterministic prog));
+  let possible_execution_list = all_choices prog in
+  List.for_all (fun sub_prog -> inclusion target (List.nth (run_rect sub_prog r) ((List.length sub_prog) - 1))) possible_execution_list
 
 let run_polymorphe (transform : transformation -> 'a -> 'a) (prog : program) (i : 'a) : 'a list =
   failwith "À compléter"
--- src/geo.ml
+++ src/geo.ml
@@ -1,4 +1,3 @@
-
 (* Code de la Section 3 du projet. *)
 
 type coord2D = {
@@ -10,23 +9,27 @@ type vector = coord2D
 type angle = float
 
 let translate (v : vector) (p : point) : point =
-  failwith "À compléter"
+  {x = p.x +. v.x ; y = p.y +. v.y}
 
 let rad_of_deg (a : angle) : angle =
-  failwith "À compléter"
+  a *. (Float.pi /. 180.0)
 
 let deg_of_rad (a : angle) : angle =
-  failwith "À compléter"
+  a *. (180.0 /. Float.pi)
 
 let rotate (c : point) (alpha : angle) (p : point) : point =
-  failwith "À compléter"
+  let theta = rad_of_deg alpha in 
+  {x = c.x +. (p.x -. c.x) *. (Float.cos theta) -. (p.y -. c.y) *. (Float.sin theta) ;
+   y = c.y +. (p.x -. c.x) *. (Float.sin theta) +. (p.y -. c.y) *. (Float.cos theta)}
   
 type transformation =
   Translate of vector
 | Rotate of point * angle
 
 let transform (t : transformation) (p : point) : point =
-  failwith "À compléter"
+  match t with
+  |Translate v -> translate v p
+  |Rotate (c,alpha) -> rotate c alpha p
 
 type rectangle = {
     x_min : float;
@@ -36,10 +39,10 @@ type rectangle = {
   }
 
 let in_rectangle (r : rectangle) (p : point) : bool =
-  failwith "À compléter"
+  (r.x_min <= p.x && r.x_max >= p.x) && (r.y_min <= p.y && r.y_max >= p.y)
 
 let corners (r :rectangle) : point list =
-  failwith "À compléter"
+  [{x = r.x_min ; y = r.y_min} ; {x = r.x_min ; y = r.y_max} ; {x = r.x_max ; y = r.y_min} ; {x = r.x_max ; y = r.y_max}]
   
 let rectangle_of_list (pl : point list) : rectangle = 
   failwith "À compléter"
