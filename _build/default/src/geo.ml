d19bfa161c52e4bc91e308e29032af142e85bbc8
Agrane Benlalam Adam <adam-christophe.agrane-benlalam@etu.u-paris.fr>
2024-10-25 20:10:10 +0200

    Implémentation des fonctions des questions suivantes : 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 4.0, 5.0 et 5.3 . J'ai également implémenté les fonctions : 4.3, 4.7 et 5.4 (tous les target_reached...). Mais elles seront surement à revoir car on ne peut pas les tester pour l'instant.

---
+++ _build/default/src/geo.ml
@@ -0,0 +1,49 @@
+
+(* Code de la Section 3 du projet. *)
+
+type coord2D = {
+    x : float;
+    y : float
+  }
+type point = coord2D
+type vector = coord2D
+type angle = float
+
+let translate (v : vector) (p : point) : point =
+  {x = p.x +. v.x ; y = p.y +. v.y}
+
+let rad_of_deg (a : angle) : angle =
+  a *. (Float.pi /. 180.0)
+
+let deg_of_rad (a : angle) : angle =
+  a *. (180.0 /. Float.pi)
+
+let rotate (c : point) (alpha : angle) (p : point) : point =
+  let theta = rad_of_deg alpha in 
+  {x = c.x +. (p.x -. c.x) *. (Float.cos theta) -. (p.y -. c.y) *. (Float.sin theta) ;
+   y = c.y +. (p.x -. c.x) *. (Float.sin theta) +. (p.y -. c.y) *. (Float.cos theta)}
+  
+type transformation =
+  Translate of vector
+| Rotate of point * angle
+
+let transform (t : transformation) (p : point) : point =
+  match t with
+  |Translate v -> translate v p
+  |Rotate (c,alpha) -> rotate c alpha p
+
+type rectangle = {
+    x_min : float;
+    x_max : float;
+    y_min : float;
+    y_max : float
+  }
+
+let in_rectangle (r : rectangle) (p : point) : bool =
+  (r.x_min <= p.x && r.x_max >= p.x) && (r.y_min <= p.y && r.y_max >= p.y)
+
+let corners (r :rectangle) : point list =
+  [{x = r.x_min ; y = r.y_min} ; {x = r.x_min ; y = r.y_max} ; {x = r.x_max ; y = r.y_min} ; {x = r.x_max ; y = r.y_max}]
+  
+let rectangle_of_list (pl : point list) : rectangle = 
+  failwith "À compléter"
